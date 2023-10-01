c3a91dc467c0f6455618b29dfdb58e03e4c065b1
Saúl Valdelvira <saul@saulv.es>
2023-10-01 20:40:17 +0200

    goto command: allow to jump to a different buffer

--- src/cmd.c
+++ src/cmd.c
@@ -15,23 +15,43 @@
 static void cmd_goto(wchar_t **args){
 	long y;
 	wchar_t *arg = args[1];
-	if (!arg){
-		 WString *number = editor_prompt(L"Line number", NULL);
-		 if (!number || wstr_length(number) == 0){
+	bool buffer = false;
+	if (arg){
+		if (wcscmp(arg, L"buffer") == 0){
+			buffer = true;
+			arg = args[2];
+		}else if (wcscmp(arg, L"line") == 0){
+			buffer = false;
+			arg = args[2];
+		}
+	}
+	if (!arg) {
+		WString *number;
+		if (buffer)
+    			number = editor_prompt(L"Buffer number", NULL);
+		else
+			number = editor_prompt(L"Line number", NULL);
+		if (!number || wstr_length(number) == 0){
 		 	wstr_free(number);
 			return;
-		 }
-		 y = wcstol(wstr_get_buffer(number), NULL, 10);
-		 wstr_free(number);
+		}
+		y = wcstol(wstr_get_buffer(number), NULL, 10);
+		wstr_free(number);
 	}else{
-		 y = wcstol(arg, NULL, 10);
-	}
-	if (y <= 0 || y > conf.num_lines){
-		editor_set_status_message(L"Invalid line number %ld", y);
-		return;
+		y = wcstol(arg, NULL, 10);
 	}
 	y--;
-	cursor_goto(conf.cx, y);
+	if (!buffer){
+		if (y < 0 || y >= conf.num_lines)
+			editor_set_status_message(L"Invalid line number %ld", y + 1);
+		else
+			cursor_goto(conf.cx, y);
+	}else{
+		if (y < 0 || y >= conf.n_buffers)
+			editor_set_status_message(L"Invalid buffer number %ld", y + 1);
+		else
+			buffer_switch(y);
+	}
 }
 
 static void cmd_search(bool forward, wchar_t **args){
