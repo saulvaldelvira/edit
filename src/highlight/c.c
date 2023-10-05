ea6867a50c8e706b623c1df86275dbaa8933adf2
Saúl Valdelvira <saul@saulv.es>
2023-10-06 00:33:51 +0200

    Basic C highlighting

--- src/highlight/c.c
+++ src/highlight/c.c
@@ -13,7 +13,24 @@ void highlight_c(void){
 			wstr_concat_cwstr(line, Color_Reset, -1);
 			continue;
 		}
-		wstr_replace(line, L"#include", Color_Purple L"#include" Color_Reset);
+
+		wstr_replace(line, L"#include ", Color_Purple L"#include " Color_IYellow);
+		wstr_replace(line, L"#define ", Color_Purple L"#define " Color_IBlue);
+		wstr_replace(line, L"#ifdef ", Color_Purple L"#ifdef " Color_IBlue);
+		wstr_replace(line, L"#ifndef ", Color_Purple L"#ifndef " Color_IBlue);
+		wstr_replace(line, L"#endif", Color_Purple L"#endif ");
+
+		wstr_replace(line, L"return", Color_Purple L"return" Color_Reset);
+		wstr_replace(line, L"if", Color_Purple L"if" Color_Reset);
+		wstr_replace(line, L"while", Color_Purple L"while" Color_Reset);
+		wstr_replace(line, L"else", Color_Purple L"else" Color_Reset);
+		wstr_replace(line, L"switch", Color_Purple L"switch" Color_Reset);
+		wstr_replace(line, L"case", Color_Purple L"case" Color_Reset);
+		wstr_replace(line, L"default", Color_Purple L"default" Color_Reset);
+		wstr_replace(line, L"break", Color_Purple L"break" Color_Reset);
+		wstr_replace(line, L"continue", Color_Purple L"continue" Color_Reset);
+		wstr_replace(line, L"goto", Color_Purple L"goto" Color_Reset);
+		wstr_concat_cwstr(line, Color_Reset, -1);
 	}
 }
 
